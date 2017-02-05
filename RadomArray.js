8893a24486ed9bd951d62ff8e4e037e8a173e7dd
hseirafi@gmail.com <hseirafi@gmail.com>
2017-02-04 17:05:18 -0800

    first commit

---
+++ RadomArray.js
@@ -0,0 +1,16 @@
+/**
+ * Created by hoss on 1/5/2015.
+ *
+ * Random Array Generation es6
+ * using Array.from() method
+ */
+
+
+const arr = Array.from(new Array(500),(x,i) => Math.round(Math.random(i)*100));
+
+
+
+
+
+
+
